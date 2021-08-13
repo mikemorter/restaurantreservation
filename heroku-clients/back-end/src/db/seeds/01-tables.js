3aefb46392a93496e2b377a693b29bc8f2767a20
Mike Morter <mikemorter@gmail.com>
2021-08-12 20:54:46 -0400

    first commit

--- heroku-clients/back-end/src/db/seeds/01-tables.js
+++
@@ -1,8 +0,0 @@
-const tables = require("./01-tables.json");
-
-exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex
-    .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
-    .then(() => knex("tables").insert(tables));
-};
